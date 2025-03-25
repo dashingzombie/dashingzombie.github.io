32e6c8662cce8da04e6565e0e5d17ba25488650b
Dev Mehrotra <dmehrotra@wisc.edu>
2025-03-25 12:23:30 -0500

    add

--- talkmap.py
+++ talkmap.py
@@ -15,10 +15,10 @@ import glob
 import getorg
 from geopy import Nominatim
 
-g = glob.glob("*.md")
+g = glob.glob("**/*.md")
 
 
-geocoder = Nominatim()
+geocoder = Nominatim(user_agent="application")
 location_dict = {}
 location = ""
 permalink = ""
